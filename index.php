ee1d209240182e7d387ef9f1c08dfe8ac79e0b91
Lanzxy159 <95922499+Lanzxy159@users.noreply.github.com>
2024-10-05 15:53:12 +0800

    index.php

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+<body>
+
+    <h1>Discriminant  of a Quadratic equation</h1>
+	 <!-- The form submits to the same page using the POST method -->
+	<form action="Results.php" method="Get">
+
+		<!-- Input field for the username. Take note of the value stored in the name attribute -->
+		<p><input type="text" value="" placeholder="Value of a here" name="a"></p>
+
+        <p><input type="text" value="" placeholder="Value of b here" name="b"></p>
+        <p><input type="text" value="" placeholder="Value of c here" name="c"></p>
+		<!-- Submit button -->
+		<p><input type="submit" value="Submit" name="getSum"></p>
+	</form>
+</body>
+</html>
